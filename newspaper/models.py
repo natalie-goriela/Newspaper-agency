a98bff9814e26a57b97f1d8cec8e612c926b24f2
Natalie Goriela <natalie.goriela@gmail.com>
2023-04-11 18:16:38 +0300

    Add models Redactor, Topic, Article

--- newspaper/models.py
+++ newspaper/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ["username"]
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    date_of_publish = models.DateTimeField(auto_now_add=True)
+    topic = models.ManyToManyField(Topic, related_name="articles")
+    publishers = models.ManyToManyField(Redactor, related_name="articles")
+
+    class Meta:
+        ordering = ["-date_of_publish"]
+
+    def __str__(self):
+        return f"{self.title}, {self.date_of_publish}"
