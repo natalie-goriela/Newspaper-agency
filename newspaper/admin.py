51e5c066db3c615fdf800758a8089b9e8911be1c
Natalie Goriela <natalie.goriela@gmail.com>
2023-04-11 18:17:53 +0300

    Amend admin.py, admin panel customized

--- newspaper/admin.py
+++ newspaper/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from newspaper.models import Redactor, Topic, Article
+
+
+@admin.register(Redactor)
+class RedactorAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("years_of_experience",)
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional info", {"fields": ("years_of_experience", )}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional info", {"fields": ("years_of_experience", )}),
+    )
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ["title", "date_of_publish"]
+    list_filter = ["date_of_publish"]
+    search_fields = ["title"]
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    search_fields = ["name"]
