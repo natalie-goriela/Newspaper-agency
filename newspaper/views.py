b2fc32e9e685c2e75ff340135c5e5407d42bbcc4
Natalie Goriela <natalie.goriela@gmail.com>
2023-04-13 09:45:48 +0300

    Add Redactor viws

--- newspaper/views.py
+++ newspaper/views.py
@@ -1,8 +1,13 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
-from newspaper.forms import ArticleForm
+from newspaper.forms import (
+    ArticleForm,
+    RedactorCreationForm,
+    RedactorUpdateForm
+)
 from newspaper.models import Redactor, Topic, Article
 
 
@@ -31,19 +36,52 @@ class ArticleDetailView(generic.DetailView):
     model = Article
 
 
-class ArticleCreateView(generic.CreateView):
+class ArticleCreateView(LoginRequiredMixin, generic.CreateView):
     model = Article
     form_class = ArticleForm
     success_url = reverse_lazy("newspaper:article-list")
 
 
-class ArticleDeleteView(generic.DeleteView):
+class ArticleDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Article
     success_url = reverse_lazy("newspaper:article-list")
 
 
-class ArticleUpdateView(generic.UpdateView):
+class ArticleUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Article
     fields = ["title", "content", "topic", "publishers"]
-    success_url = reverse_lazy("newspaper:article-list")
+    success_url = reverse_lazy("newspaper:article-detail")
+
+
+class RedactorListView(generic.ListView):
+    model = Redactor
+    paginate_by = 3
+
+
+class RedactorDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Redactor
+    queryset = Redactor.objects.all().prefetch_related("articles__topic")
+
+
+class RedactorCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Redactor
+    form_class = RedactorCreationForm
+    success_url = reverse_lazy("newspaper:redactor-list")
+
+
+class RedactorUpdateView(generic.UpdateView):
+    model = Redactor
+    form_class = RedactorUpdateForm
+
+    def get_success_url(self):
+        return reverse_lazy(
+            "newspaper:redactor-detail",
+            kwargs={'pk': self.kwargs['pk']}
+        )
+
+
+class RedactorDeleteView(generic.DeleteView):
+    model = Redactor
+    success_url = reverse_lazy("newspaper:redactor-list")
+
 
