0f5a3766299c16384229cd7e297d8c27c3df4099
Natalie Goriela <natalie.goriela@gmail.com>
2023-04-13 09:46:21 +0300

    Add Redactor forms

--- newspaper/forms.py
+++ newspaper/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
 
-from newspaper.models import Article, Topic
+from newspaper.models import Article, Topic, Redactor
 
 
 class ArticleForm(forms.ModelForm):
@@ -18,3 +19,23 @@ class ArticleForm(forms.ModelForm):
     class Meta:
         model = Article
         fields = ["title", "content", "topic", "publishers"]
+
+
+class RedactorCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = Redactor
+        fields = UserCreationForm.Meta.fields + (
+            "first_name",
+            "last_name",
+            "years_of_experience"
+        )
+
+
+class RedactorUpdateForm(forms.ModelForm):
+    class Meta(UserCreationForm.Meta):
+        model = Redactor
+        fields = UserCreationForm.Meta.fields + (
+            "first_name",
+            "last_name",
+            "years_of_experience"
+        )
